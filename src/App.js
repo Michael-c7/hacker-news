65acd24a5451fde75908d5849ceeaf3efddf2057
Michael Carr <mcarr2468@gmail.com>
2021-11-03 14:29:37 -0500

    V1.0 init files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  let url = "https://hn.algolia.com/api/v1/search?query=javascript"
+  const getData = async _ => {
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data)
+  }
+
+  // getData()
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello world
     </div>
   );
 }
