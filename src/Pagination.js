cebf78c354114c3d81f231bc42e674464f2a1672
Michael Carr <mcarr2468@gmail.com>
2021-11-05 18:55:50 -0500

    V1.3 got the prev & next buttons working

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,15 +1,42 @@
 import React from 'react'
+import { useGlobalContext } from './context'
 
 const Pagination = () => {
+    const {
+        currentPage,
+        setCurrentPage,
+        totalAmountOfPages,
+    } = useGlobalContext()
+
+    const changeCurrentPage = direction => {
+        if(direction === "previous") {
+            if(currentPage <= 1) {
+                setCurrentPage(totalAmountOfPages)
+            } else {
+                setCurrentPage(currentPage - 1)
+            }
+
+        } 
+
+
+        if(direction === "next") {
+            if(currentPage >= totalAmountOfPages) {
+                setCurrentPage(0)
+            } else {
+                setCurrentPage(currentPage + 1)
+            }
+        }
+    }
+
     return (
         <section className="pagination">
-            <button className="pagination__prev">Prev</button>
+            <button className="pagination__prev" onClick={() => changeCurrentPage("previous")}>Prev</button>
                 <div className="pagination__page-info">
-                    <span>1</span>
+                    <span>{currentPage}</span>
                     <span> of </span>
-                    <span>50</span>
+                    <span>{totalAmountOfPages}</span>
                 </div>
-            <button className="pagination__next">Next</button>
+            <button className="pagination__next"  onClick={() => changeCurrentPage("next")}>Next</button>
         </section>
     )
 }
